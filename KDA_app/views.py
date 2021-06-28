ee75c18adc092b56e0e07f49207bc0b8d7c108fb
mhz038 <hamza.zaid29@yahoo.com>
2021-06-28 11:06:15 +0500

    new

--- KDA_app/views.py
+++ KDA_app/views.py
@@ -173,8 +173,8 @@ def appointment_list(request):
     appoin_list = Appointment.objects.all()
     delete_old_slots(current_time.date())
     for obj in appoin_list:
-        if obj.child.child_form:
-            print("YESS")
+        if obj.child.child_form.count():
+            print("yess")
         else:
             print("NO")
     if request.method == "POST":
